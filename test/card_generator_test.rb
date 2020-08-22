2f7711bbddde92cf7ba1e61ff695c9adca6e517e
Roberto Basulto <roberto.basulto085@gmail.com>
2020-08-22 15:30:37 -0400

    Sucessfuly returning Card objects and added to runner

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,4 +1,3 @@
-
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/card_generator"
@@ -6,10 +5,25 @@ require "./lib/card_generator"
 class CradGeneratorTest < Minitest::Test
 
   def test_it_exists
-    file_name = "cards.txt"
-    card_generator = CardGenerator.new(file_name)
+    filename = "lib/cards.txt"
+    cards = CardGenerator.new(filename)
 
-    assert_instance_of CardGenerator, card_generator
+    assert_instance_of CardGenerator, cards
+  end
+
+  def test_should_return_the_file_name
+    filename = "lib/cards.txt"
+    cards = CardGenerator.new(filename)
+
+    assert_equal "lib/cards.txt", cards.filename
+  end
+
+  def test_should_return_array_of_cards
+    filename = "lib/cards.txt"
+    cards = CardGenerator.new(filename)
+
+
+    assert_equal 52, cards.cards.length
   end
 
 end
